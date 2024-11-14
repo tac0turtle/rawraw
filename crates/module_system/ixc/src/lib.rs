6c27f0ce9f5c0e378202f4124f75d20e79c72f4c
Marko Baricevic <marko@baricevic.me>
2024-11-14 17:01:00 +0100

    fix stuff

---
+++ crates/module_system/ixc/src/lib.rs
@@ -0,0 +1,35 @@
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+
+#[doc(inline)]
+pub use ixc_core::resource::Resources;
+#[doc(inline)]
+pub use ixc_core::{bail, create_account, ensure, error, Context, EventBus, Result, Service};
+
+#[doc(inline)]
+pub use ixc_message_api::AccountID;
+#[doc(inline)]
+pub use ixc_schema::{Bytes, Str};
+#[doc(inline)]
+pub use simple_time::{Duration, Time};
+#[doc(inline)]
+pub use state_objects::{Accumulator, AccumulatorMap, Item, Map};
+
+pub use ixc_core as core;
+pub use ixc_message_api as message_api;
+pub use ixc_schema as schema;
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate ixc_core_macros;
+#[doc(inline)]
+pub use ixc_core_macros::{handler, handler_api, on_create, publish, Resources};
+
+#[allow(unused_imports)]
+#[macro_use]
+extern crate ixc_schema_macros;
+#[doc(inline)]
+pub use ixc_schema_macros::*;
+
+#[allow(unused_imports)]
+#[doc(inline)]
+pub use state_objects_macros::*;
