7f881a28e7c787e93992823be95feac4fb338e3c
Aaron Craelius <aaronc@users.noreply.github.com>
2024-11-21 14:14:17 -0500

    add owned data benchmark

--- crates/module_system/schema/benches/basic.rs
+++ crates/module_system/schema/benches/basic.rs
@@ -6,7 +6,8 @@ fn main() {
     divan::main();
 }
 
-mod schema_binary_bench {
+/// Native binary benchmarks with borrowed strings and slices.
+mod schema_binary_borrowed {
     use ixc_schema::binary::NativeBinaryCodec;
     use ixc_schema::codec::{decode_value, Codec};
     use ixc_schema::mem::MemoryManager;
@@ -42,6 +43,68 @@ mod schema_binary_bench {
             ],
         }
     }
+    #[divan::bench]
+    fn encode() {
+        let msg = msg1();
+        let mem = MemoryManager::new();
+        divan::black_box(NativeBinaryCodec.encode_value(&msg, &mem).unwrap());
+    }
+
+    #[divan::bench]
+    fn decode(bencher: divan::Bencher) {
+        let msg = msg1();
+        let mem = MemoryManager::new();
+        let bz = NativeBinaryCodec.encode_value(&msg, &mem).unwrap();
+        bencher.bench(|| {
+            let mem2 = MemoryManager::new();
+            let msg2 = decode_value::<MsgSend>(&NativeBinaryCodec, bz, &mem2).unwrap();
+            assert_eq!(msg, msg2);
+        });
+    }
+}
+
+/// Native binary benchmarks with owned strings and slices.
+mod schema_binary_owned {
+    extern crate alloc;
+
+    use alloc::string::String;
+    use alloc::vec::Vec;
+    use ixc_schema::binary::NativeBinaryCodec;
+    use ixc_schema::codec::{decode_value, Codec};
+    use ixc_schema::mem::MemoryManager;
+    use ixc_schema_macros::SchemaValue;
+
+    #[derive(SchemaValue, Default, PartialEq, Eq, Debug)]
+    #[sealed]
+    struct MsgSend {
+        from: String,
+        to: String,
+        amount: Vec<Coin>,
+    }
+    #[derive(SchemaValue, Default, PartialEq, Eq, Debug)]
+    #[sealed]
+    struct Coin {
+        denom: String,
+        amount: u128,
+    }
+
+    fn msg1() -> MsgSend {
+        MsgSend {
+            from: "foo".to_string(),
+            to: "bar".to_string(),
+            amount: vec![
+                Coin {
+                    denom: "uatom".to_string(),
+                    amount: 1234567890,
+                },
+                Coin {
+                    denom: "foo".to_string(),
+                    amount: 9876543210,
+                },
+            ],
+        }
+    }
+
     #[divan::bench]
     fn encode() {
         let msg = msg1();
