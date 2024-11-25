f61465b6fddcee2cb6ec05b01bf43ff88c1b7c8e
unknown unknown <unknown@unknown>
2024-11-25 17:19:51 +0100

    checkpoint

---
+++ crates/server/db/src/jmt.rs
@@ -0,0 +1,12 @@
+use jmt::SimpleHasher;
+use jmt::storage::TreeReader;
+
+pub struct Jmt<R: TreeReader> {
+    tree_reader: R,
+}
+
+impl<R: TreeReader> Jmt<R> {
+    pub fn new() -> Self {
+        panic!("")
+    }
+}
--- crates/server/db/src/lib.rs
+++ crates/server/db/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod sled;
 mod tracer;
+mod jmt;
 
 pub trait State {
     fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, String>;
