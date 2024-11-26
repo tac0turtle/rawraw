c08c3bee7662847ae5c480eafa0fe9a9b86beefa
Aaron Craelius <aaronc@users.noreply.github.com>
2024-11-26 16:26:22 -0500

    WIP

--- crates/dsl/dsl/src/main.rs
+++ crates/dsl/dsl/src/main.rs
@@ -1,98 +1,105 @@
-//! Custom DSL parser.
+#![allow(missing_docs)]
 
 mod lex;
 use logos::Logos;
 use std::io::Read;
 use std::ops::Range;
-//
-// struct File {
-//     items: Vec<Item>,
-// }
-//
-// enum Item {
-//     Interface(ItemInterface),
-//     Handler(Handler),
-//     Impl(Impl),
-// }
-//
-// struct ItemInterface {
-//     name: String,
-//     items: Vec<InterfaceItem>,
-// }
-//
-// struct Ident {
-//     value: String,
-//     span: Span,
-// }
-//
-// type Span = Range<usize>;
-//
-// enum InterfaceItem {
-//     Fn(InterfaceItemFn),
-//     // Event(Struct),
-//     // Struct(Struct),
-//     // Enum(Enum),
-// }
-//
-// struct InterfaceItemFn {
-//     name: String,
-//     fn_type: FnType,
-//     visibility: Visibility,
-//     fn_args: Vec<FnArg>,
-// }
-//
-// struct FnArg {
-//     name: String,
-//     ty: String,
-//     key: bool,
-// }
-//
-// enum FnType {
-//     Tx,
-//     Query,
-//     Pure,
-// }
-//
-// enum Visibility {
-//     Public,
-//     Private,
-// }
-//
-// struct Handler {}
-//
-// struct Impl {}
-//
-// enum HandlerItem {
-//     Collection(),
-//     Client(),
-// }
-//
-// struct Block {
-//     stmts: Vec<Stmt>,
-// }
-//
-// enum Stmt {
-//     Local(Local),
-//     Expr(Expr),
-// }
-//
-// struct Local {
-//     name: String,
-//     ty: String,
-// }
-//
-// enum Expr {
-//     Call(ExprCall),
-// }
-//
-// struct ExprCall {
-//     name: String,
-// }
-//
-// fn parse(input: &mut &str) -> PResult<File> {
-//
-//     Ok(File { items: vec![] })
-// }
+use pest::Parser;
+use pest_derive::Parser;
+
+
+struct File {
+    items: Vec<Item>,
+}
+
+enum Item {
+    Interface(ItemInterface),
+    Handler(Handler),
+    Impl(Impl),
+}
+
+struct ItemInterface {
+    name: String,
+    items: Vec<InterfaceItem>,
+}
+
+struct Ident {
+    value: String,
+    span: Span,
+}
+
+struct Span(Range<usize>);
+
+enum InterfaceItem {
+    Fn(InterfaceItemFn),
+    // Event(Struct),
+    // Struct(Struct),
+    // Enum(Enum),
+}
+
+struct InterfaceItemFn {
+    name: String,
+    fn_type: FnType,
+    visibility: Visibility,
+    fn_args: Vec<FnArg>,
+}
+
+struct FnArg {
+    name: String,
+    ty: String,
+    key: bool,
+}
+
+enum FnType {
+    Tx,
+    Query,
+    Pure,
+}
+
+enum Visibility {
+    Public,
+    Private,
+}
+
+struct Handler {}
+
+struct Impl {}
+
+enum HandlerItem {
+    Collection(),
+    Client(),
+}
+
+struct Block {
+    stmts: Vec<Stmt>,
+}
+
+enum Stmt {
+    Local(Local),
+    Expr(Expr),
+}
+
+struct Local {
+    name: String,
+    ty: String,
+}
+
+enum Expr {
+    Call(ExprCall),
+}
+
+struct ExprCall {
+    name: String,
+}
+
+fn parse(input: &str) -> Result<File, anyhow::Error> {
+    let res = DSLParser::parse(Rule::file, &input)?;
+    for pair in res {
+        pair.as_rule();
+    }
+    Ok(File { items: vec![] })
+}
+
 //
 // fn rust_codegen(ast: File) -> anyhow::Result<syn::File> {
 //     Ok(syn::File{
@@ -102,12 +109,18 @@ use std::ops::Range;
 //     })
 // }
 //
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct DSLParser;
+
 fn compile() -> anyhow::Result<()> {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input)?;
-    for (token, span) in lex::Token::lexer(&input).spanned() {
-        println!("{:?} {:?}", token, span);
-    }
+    parse(&input)?;
+    // for (token, span) in lex::Token::lexer(&input).spanned() {
+    //     println!("{:?} {:?}", token, span);
+    // }
     // let ast = parse(&mut input.as_str())
     //     .map_err(|e| anyhow::anyhow!("Parse error: {}", e))?;
     // let syn_ast = rust_codegen(ast)?;
