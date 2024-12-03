b61818c07114afe4d9410c89036cf6c264f367be
Aaron Craelius <aaronc@users.noreply.github.com>
2024-12-03 10:58:43 -0500

    update vm api

--- crates/vm/api/src/lib.rs
+++ crates/vm/api/src/lib.rs
@@ -1,10 +1,13 @@
 //! **WARNING: This is an API preview! Expect major bugs, glaring omissions, and breaking changes!**
 //!
 //! Virtual Machine API
+#![no_std]
+
+use ixc_message_api::AccountID;
 use ixc_message_api::code::ErrorCode;
 use ixc_message_api::handler::{Allocator, HostBackend};
 use ixc_message_api::packet::MessagePacket;
-
+use allocator_api2::vec::Vec;
 
 /// A virtual machine that can run message handlers.
 pub trait VM {
@@ -23,6 +26,7 @@ pub trait VM {
         allocator: &dyn Allocator,
     ) -> Result<(), ErrorCode>;
 
+    /// Runs a query handler with the provided message packet and host backend.
     fn run_query<S: ReadonlyStore>(
         &self,
         store: &S,
@@ -32,6 +36,7 @@ pub trait VM {
         allocator: &dyn Allocator,
     ) -> Result<(), ErrorCode>;
 
+    /// Runs a system message handler with the provided message packet and host backend.
     fn run_system_message<S: ReadonlyStore>(
         &self,
         store: &S,
@@ -42,10 +47,13 @@ pub trait VM {
     ) -> Result<(), ErrorCode>;
 }
 
+/// A store that can only be read from.
+/// In the context of a VM,
+/// this state should only be used to retrieve the code for a handler from the store.
 pub trait ReadonlyStore {
-    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, ErrorCode>;
+    /// Gets the value for the given key for the given account.
+    fn get<A: Allocator>(&self, account_id: AccountID, key: &[u8], allocator: A) -> Result<Option<Vec<u8, A>>, ErrorCode>;
 }
-
 
 /// A descriptor for a handler.
 #[non_exhaustive]
